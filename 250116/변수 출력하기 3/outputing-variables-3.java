a05b077753cf5a5f2620ab01be5db787588a5512
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-16 11:00:25 +0900

    Title: 변수 출력하기 3, Time: 98ms, Memory: 9MB, Status: Passed - CodeTree

---
+++ 250116/변수 출력하기 3/outputing-variables-3.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int a = 1;
+        int b = 2;
+        char c = 'C';
+        String d = "->";
+        System.out.printf("%d%s%d%s%c", a, d, b, d, c);
+    }
+}
