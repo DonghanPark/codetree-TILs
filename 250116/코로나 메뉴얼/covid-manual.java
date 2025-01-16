96346089ecad9917c8b0edec08ecfe93b5248da5
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-16 21:27:57 +0900

    Title: 코로나 메뉴얼, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

--- 250116/코로나 메뉴얼/covid-manual.java
+++ 250116/코로나 메뉴얼/covid-manual.java
@@ -6,20 +6,26 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         char sick;
-        int temp;
+        int temp, emer = 0;
 
         for(int i = 0; i < 3; i++) {
             sick = sc.next().charAt(0);
             temp = sc.nextInt();
 
             if (sick == 'Y' && temp >= 37)
-                System.out.print('A');
-            else if (sick == 'N' && temp >= 37)
-                System.out.print('B');
-            else if (sick == 'Y' && temp < 37)
-                System.out.print('C');
-            else
-                System.out.print('D');
+                // System.out.print('A');
+                emer += 1;
+            // else if (sick == 'N' && temp >= 37)
+            //     System.out.print('B');
+            // else if (sick == 'Y' && temp < 37)
+            //     System.out.print('C');
+            // else
+            //     System.out.print('D');
         }
+
+        if (emer >= 2)
+            System.out.print('E');
+        else
+            System.out.print('N');
     }
 }
