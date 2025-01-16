063b5734b7ab317c2acb2c7c3695a1945624ecd9
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-16 21:25:24 +0900

    Title: 코로나 메뉴얼, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250116/코로나 메뉴얼/covid-manual.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        char sick;
+        int temp;
+
+        for(int i = 0; i < 3; i++) {
+            sick = sc.nextChar();
+            temp = sc.nextInt();
+
+            if (sick == 'Y' && temp >= 37)
+                System.out.print('A');
+            else if (sick == 'N' && temp >= 37)
+                System.out.print('B');
+            else if (sick == 'Y' && temp < 37)
+                System.out.print('C');
+            else
+                System.out.print('D');
+        }
+    }
+}
