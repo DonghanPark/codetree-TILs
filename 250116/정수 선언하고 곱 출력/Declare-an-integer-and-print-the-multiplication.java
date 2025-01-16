7be6c74166221e731dba43b473853136d89868fc
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-16 10:52:27 +0900

    Title: 정수 선언하고 곱 출력, Time: 96ms, Memory: 9MB, Status: Passed - CodeTree

--- 250116/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
+++ 250116/정수 선언하고 곱 출력/Declare-an-integer-and-print-the-multiplication.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         int a = 26;
         int b = 5;
-        System.out.print(a + " * " + b + " = " + a+b);
+        System.out.print(a + " * " + b + " = " + a*b);
     }
 }
