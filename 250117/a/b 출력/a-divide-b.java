7a845ebb9f60ff6492ebcb0f96aa06e22674d2ae
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-17 18:23:10 +0900

    Title: a/b 출력, Time: 133ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250117/a/b 출력/a-divide-b.java
+++ 250117/a/b 출력/a-divide-b.java
@@ -10,7 +10,7 @@ public class Main {
         a = sc.nextInt();
         b = sc.nextInt();
 
-        answer = (a/b*10000000000)/10000000000.0;
+        answer = (a/b*10000000000.0)/10000000000.0;
         System.out.print(answer);
     }
 }
