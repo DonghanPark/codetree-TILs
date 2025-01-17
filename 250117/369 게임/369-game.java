1ecd010e3e1e67dd7ad177893adc3ce4bbc06fd7
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-17 19:06:59 +0900

    Title: 369 게임, Time: 134ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250117/369 게임/369-game.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+
+        for (int i = 1; i <= n; i++) {
+            if (isThreeTimes(i) || contains369(i))
+                System.out.print(0);
+            else
+                System.out.printf("%d ", i);
+        }
+    }
+
+    public static boolean isThreeTimes(int num) {
+        if (num % 3 == 0)
+            return true;
+        
+        return false;
+    }
+
+    public static boolean contains369(int num) {
+        while(num != 0) {
+            if ((num % 10) % 3 == 0)
+                return true;
+            num /= 10;
+        }
+        return false;
+    }
+}
