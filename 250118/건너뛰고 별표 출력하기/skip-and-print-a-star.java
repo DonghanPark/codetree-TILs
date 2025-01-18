4a96dd4732594e917640970b145487be30eac4ed
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-18 23:34:19 +0900

    Title: 건너뛰고 별표 출력하기, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250118/건너뛰고 별표 출력하기/skip-and-print-a-star.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = nextInt();
+        int count = 1;
+
+        for (int i = 0; i < 2*n-1; i++) {
+            for (int j = 0; j < count; j++) {
+                System.out.print("*");
+            }
+            System.out.print("\n\n");
+            if (count < 4)
+                count++;
+            else
+                count--;
+        }
+    }
+}
