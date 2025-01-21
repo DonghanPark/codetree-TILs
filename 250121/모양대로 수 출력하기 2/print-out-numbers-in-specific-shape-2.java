41b00fd8eb2f6cdb745a56b5ad399c53d8279d51
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-01-21 20:21:43 +0900

    Title: 모양대로 수 출력하기 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

---
+++ 250121/모양대로 수 출력하기 2/print-out-numbers-in-specific-shape-2.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int count = 1;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                System.out.print("%d ", count*2);
+                
+                count++;
+                if (count > 4)
+                    count = 1;
+            }
+        }
+    }
+}
