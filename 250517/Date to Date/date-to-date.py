d3864d31a8510c3515bd668d7fa43665af40b6f5
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-17 14:20:26 +0900

    Title: Date to Date, Time: 43ms, Memory: 16MB, Status: Passed - Codetree

--- 250517/Date to Date/date-to-date.py
+++ 250517/Date to Date/date-to-date.py
@@ -1,7 +1,7 @@
 m1, d1, m2, d2 = tuple(map(int, input().split()))
 
 #              0  1   2   3   4   5   6   7   8   9   10  11  12
-num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+num_of_days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
 def cal_days(m, d):
     return sum(num_of_days[:m]) + d
