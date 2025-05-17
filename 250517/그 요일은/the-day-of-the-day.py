fa377117a816290d8d38da2c7e9743cd2cffcd9c
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-17 14:47:17 +0900

    Title: 그 요일은, Time: 41ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250517/그 요일은/the-day-of-the-day.py
@@ -0,0 +1,16 @@
+m1, d1, m2, d2 = tuple(map(int, input().split()))
+string = input()
+days_of_weak = {"Mon":0, "Tue":1, "Wed":2, "Thu":3, "Fri":4, "Sat":5, "Sun":6}
+#              0  1   2   3   4   5   6   7   8   9   10  11  12
+num_of_days = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+
+def cal_days(m, d):
+    return sum(num_of_days[:m]) + d
+
+days = cal_days(m2, d2) - cal_days(m1, d1)
+weeks = days // 8
+
+if days % 7 >= days_of_weak[string]:
+    print(weeks + 1)
+else:
+    print(weeks)
