89cb7a3db726568ca1c6b408809b033a82e5c8ca
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-17 16:15:21 +0900

    Title: 신기한 타일 뒤집기, Time: 64ms, Memory: 18MB, Status: Passed - Codetree

--- 250517/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250517/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,17 +1,38 @@
-n = int(input())
-MAX_SIZE = 200000
-tile = [0] * (MAX_SIZE + 1)
-DIRECTION_OFFSET = {"R":1, "L":-1}
-location = 100000
+def parse_input() -> list[tuple[int, str]]:
+    n = int(input())
+    moves = []
+    DIRECTION_OFFSET = {"R":1, "L":-1}
 
-for _ in range(n):
-    num, way = tuple(input().split())
-    num = int(num)
+    for _ in range(n):
+        steps, direction = tuple(input().split())
+        moves.append((int(steps), DIRECTION_OFFSET.get(direction)))
 
-    next_location = location + DIRECTION_OFFSET[way] * (num - 1)
-    for i in range(location, next_location + DIRECTION_OFFSET[way], DIRECTION_OFFSET[way]):
-        tile[i] = DIRECTION_OFFSET[way]
+    return moves
+
+def apply_moves(moves: list[tuple[int, str]]) -> list[int]:
+    MAX_SIZE = 200000
+    tile_state = [0] * (MAX_SIZE + 1)
+    curr_pos = MAX_SIZE // 2
+
+    for steps, direction in moves:
+        next_pos = curr_pos + (steps - 1) * direction
+        for i in range(curr_pos, next_pos + direction, direction):
+            tile_state[i] = direction
+        curr_pos = next_pos
     
-    location = next_location
-# print(tile)
-print(tile.count(-1), tile.count(1))
+    return tile_state
+
+def calc_tile(tile_state: list[int]) -> tuple[int, int]:
+    WHITE = -1
+    BLACK = 1
+
+    count_of_while = tile_state.count(WHITE)
+    count_of_black = tile_state.count(BLACK)
+
+    return count_of_while, count_of_black
+
+moves = parse_input()
+tile_state = apply_moves(moves)
+count_of_while, count_of_black = calc_tile(tile_state)
+
+print(count_of_while, count_of_black)
