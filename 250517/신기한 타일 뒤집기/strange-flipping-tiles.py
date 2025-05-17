7b832c6e527714f6bc07cb405d98dc9a4c76b89f
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-17 15:46:48 +0900

    Title: 신기한 타일 뒤집기, Time: 91ms, Memory: 19MB, Status: Runtime Error - Codetree

--- 250517/신기한 타일 뒤집기/strange-flipping-tiles.py
+++ 250517/신기한 타일 뒤집기/strange-flipping-tiles.py
@@ -1,13 +1,8 @@
 n = int(input())
-MAX_SIZE = 1000
+MAX_SIZE = 2000
 tile = [0] * (MAX_SIZE + 1)
 DIRECTION_OFFSET = {"R":1, "L":-1}
-location = 1
-
-def calc_next_location(location, num, way):
-    DIRECTION_OFFSET = {"R":1, "L":-1}
-    OFFSET = DIRECTION_OFFSET[way] * num
-    return location + OFFSET
+location = 1000
 
 for _ in range(n):
     num, way = tuple(input().split())
@@ -18,5 +13,5 @@ for _ in range(n):
         tile[i] = DIRECTION_OFFSET[way]
     
     location = next_location
-
+# print(tile)
 print(tile.count(-1), tile.count(1))
