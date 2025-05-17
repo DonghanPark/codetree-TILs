69b799a46f4e17ad409250e709ffd1ef540b0477
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-17 17:48:16 +0900

    Title: 벌금은 누구에게, Time: 68ms, Memory: 16MB, Status: Passed - Codetree

--- 250517/벌금은 누구에게/who-will-pay.py
+++ 250517/벌금은 누구에게/who-will-pay.py
@@ -1,21 +1,15 @@
 n, m, k = tuple(map(int, input().split()))
+penalties = [int(input()) for _ in range(m)]
 
-student = [k] * (n + 1)
+def find_first_fine_student(penalties: list[int]) -> int:
+    student = [k] * (n + 1)
+    for penalty in penalties:
+        student[penalty] -= 1
 
-def check_fine_student(student: list[int]) -> int:
-    for i in range(1, n + 1):
-        if student[i] == 0:
-            return i
-    
-    return 0
+        if student[penalty] == 0:
+            return penalty
 
-fine_student = -1
-for _ in range(m):
-    penalty = int(input())
-    student[penalty] -= 1
+    return -1
 
-    if student.count(0) > 0:
-        fine_student = check_fine_student(student)
-        break
-
-print(fine_student)
+first_fine_student = find_first_fine_student(penalties)
+print(first_fine_student)
