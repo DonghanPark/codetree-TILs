a89fb1ed4d0733021ec62849bf1ca32ac319ac06
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-21 01:30:37 +0900

    Title: 밭의 높이를 고르게하기, Time: 54ms, Memory: 16MB, Status: Passed - Codetree

--- 250521/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
+++ 250521/밭의 높이를 고르게하기/equalizing-the-height-of-the-field.py
@@ -3,9 +3,9 @@ n, h, t = tuple(map(int, input().split()))
 arr = list(map(int, input().split()))
 
 min_charge = sys.maxsize
-for i in range(n-t):
+for i in range(n - t + 1):
     charge = 0
-    for j in range(i, i+t):
+    for j in range(i, i + t):
         charge += abs(h-arr[j])
     
     if charge < min_charge:
