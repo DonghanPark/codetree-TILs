313c4224d2739c701fa7921c8b05881b2bb484a2
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-05-20 23:50:57 +0900

    Title: 특정 구간의 원소 평균값, Time: 67ms, Memory: 17MB, Status: Passed - Codetree

---
+++ 250520/특정 구간의 원소 평균값/elemental-mean-value-for-a-particular-interval.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = list(map(int, input().split()))
+count = 0
+for i in range(n):
+    for j in range(i, n):
+        avg = sum(arr[i:j+1]) / (j-i+1)
+        if avg in arr[i:j+1]:
+            count += 1
+
+print(count)
