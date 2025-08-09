059b443f55dec76a92ca04289bc1cc1ba9bf15d7
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-08-09 23:33:11 +0900

    Title: 벽이 있는 충돌 실험, Time: 3086ms, Memory: 80MB, Status: Time Limit Exceed - Codetree

---
+++ 250809/벽이 있는 충돌 실험/collision-experiment-with-wall.java
@@ -0,0 +1,85 @@
+import java.util.Scanner;
+import java.util.Map;
+import java.util.HashMap;
+public class Main {
+    static class Marble {
+        int x;
+        int y;
+        int d;
+
+        Marble(int x, int y, int d) {
+            this.x = x;
+            this.y = y;
+            this.d = d;
+        }
+    }
+
+    public static void main(String[] args) {
+        Map<Character, Integer> direction = new HashMap<>();
+        direction.put('U', 0);
+        direction.put('D', 1);
+        direction.put('R', 2);
+        direction.put('L', 3);
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, 1, -1};
+
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while(T-- > 0) {
+            int N = sc.nextInt();
+            int M = sc.nextInt();
+            
+            final int PERIOD = 2 * N - 1;
+            int removedMarbles = 0;
+
+            Marble[] marbles = new Marble[M];
+            for (int i = 0; i < M; i++) {
+                int x = sc.nextInt()-1;
+                int y = sc.nextInt()-1;
+                char d = sc.next().charAt(0);
+                marbles[i] = new Marble(x, y, direction.get(d));
+            }
+
+            for (int i = 0; i < PERIOD; i++) {
+                // move
+                int[][] grid = new int[N][N];
+                for (int j = 0; j < M; j++) {
+                    int nx = marbles[j].x + dx[marbles[j].d];
+                    int ny = marbles[j].y + dy[marbles[j].d];
+                    
+                    // valid range
+                    if (nx >= 0 && nx < N && ny >= 0 && ny < N) { 
+                        marbles[j].x = nx;
+                        marbles[j].y = ny;
+                        grid[nx][ny] += 1;
+                    }
+                    // change direction
+                    else {
+                        if (marbles[j].d >= direction.get('R')) {
+                            marbles[j].d = (marbles[j].d + 1) % 2 + 2;
+                        }
+                        else {
+                            marbles[j].d = (marbles[j].d + 1) % 2;
+                        }
+                        if (marbles[j].x < N && marbles[j].y < N)
+                            grid[marbles[j].x][marbles[j].y] += 1;
+                    }
+                }
+
+                // check collision
+                for (int j = 0; j < M; j++) {
+                    int x = marbles[j].x;
+                    int y = marbles[j].y;
+
+                    if (x < N && y < N && grid[x][y] > 1) {
+                        marbles[j].x = N;
+                        marbles[j].y = N;
+                        removedMarbles++;
+                    }
+                }
+            }
+
+            System.out.println(M - removedMarbles);
+        }
+    }
+}
