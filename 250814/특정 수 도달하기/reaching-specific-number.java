4f8e9837936c18b9717da7af836a8200d8dea042
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-08-14 22:54:05 +0900

    Title: 특정 수 도달하기, Time: 89ms, Memory: 9MB, Status: Wrong Answer - Codetree

---
+++ 250814/특정 수 도달하기/reaching-specific-number.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int stopIndex = -1;
+        int sum = 0;
+        int[] array = new int[10];
+        for (int i = 0; i < 10; i++) {
+            array[i] = Integer.parseInt(st.nextToken());
+            
+            if (array[i] >= 250) {
+                stopIndex = i;
+                break;
+            }
+            
+            sum += array[i];
+        }
+
+        if (stopIndex != -1) {
+            System.out.printf("%d %.1f", sum, (double)sum / stopIndex);
+        }
+        else {
+            System.out.println(sum + " " + sum / 10);
+        }
+
+    }
+}
