8e9456480344966454e94e48552240c25ad2ce0a
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-08-15 19:05:47 +0900

    Title: 갈 수 있는 곳들, Time: 103ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 250815/갈 수 있는 곳들/places-can-go.java
@@ -0,0 +1,64 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static class Pair {
+        int x, y;
+
+        public Pair(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        int[][] grid = new int[N][N];
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                grid[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        int[][] visited = new int[N][N];
+        Queue<Pair> queue = new ArrayDeque<>();
+        for (int i = 0; i < K; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken()) - 1;
+            int y = Integer.parseInt(st.nextToken()) - 1;
+            
+            queue.offer(new Pair(x, y));
+            visited[x][y] = 1;
+        }
+
+        int[] dx = new int[]{-1, 1, 0, 0};
+        int[] dy = new int[]{0, 0, -1, 1};
+        int count = 0;
+        while(!queue.isEmpty()) {
+            Pair point = queue.poll();
+            count++;
+            int x = point.x;
+            int y = point.y;
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+
+                if (nx < 0 || nx >= N || ny < 0 || ny >= N) continue;
+                if (grid[nx][ny] == 1) continue;
+                if (visited[nx][ny] == 1) continue;
+
+                visited[nx][ny] = 1;
+                queue.offer(new Pair(nx, ny));
+            }
+        }
+
+        System.out.println(count);
+    }
+}
