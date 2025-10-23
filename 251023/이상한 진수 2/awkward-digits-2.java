0fdc2ac30b3e0e5c73cc4d667bac3dd5334513e2
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2025-10-23 22:53:35 +0900

    Title: 이상한 진수 2, Time: 121ms, Memory: 10MB, Status: Passed - Codetree

--- 251023/이상한 진수 2/awkward-digits-2.java
+++ 251023/이상한 진수 2/awkward-digits-2.java
@@ -3,8 +3,12 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
-        if (a.equals(a.replaceFirst("0", "1")))
-            a = a.replaceFirst("1", "0");
+        if (a.equals(a.replaceFirst("0", "1"))) {
+            int lastIndex = a.lastIndexOf("1");
+            String part1 = a.substring(0, lastIndex);
+            String part2 = a.substring(lastIndex + 1);
+            a = part1 + "0" + part2;
+        }
         else
             a = a.replaceFirst("0", "1");
 
