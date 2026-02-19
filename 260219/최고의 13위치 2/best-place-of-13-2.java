22b49576ad9e639db993ee2445685d264b269236
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2026-02-19 22:33:02 +0900

    Title: 최고의 13위치 2, Time: 138ms, Memory: 10MB, Status: Passed - Codetree

--- 260219/최고의 13위치 2/best-place-of-13-2.java
+++ 260219/최고의 13위치 2/best-place-of-13-2.java
@@ -27,13 +27,14 @@ public class Main {
 
         if (n >= 6) {
             for (int i = 0; i < n; i++) {
-                for (int j = 0; j < n - 6 + 1; j++) {
-                    for (int k = 0; k < 3; k++) {
-                        sum = sum + arr[i][j + k] + arr[i][j + 3 + k];
+                for (int j = 0; j <= n - 3; j++) {
+                    for (int l = j + 3; l <= n - 3; l++) {
+                        for (int m = 0; m < 3; m++) {
+                            sum += arr[i][j + m] + arr[i][l + m];
+                        }
+                        maxSum = Math.max(maxSum, sum);
+                        sum = 0;
                     }
-
-                    maxSum = Math.max(maxSum, sum);
-                    sum = 0;
                 }
             }
         }
