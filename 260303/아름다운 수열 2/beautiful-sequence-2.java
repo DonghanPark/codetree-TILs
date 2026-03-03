5dfbade11bdc93847fa9fab2a828bef6ee326bba
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2026-03-03 19:51:31 +0900

    Title: 아름다운 수열 2, Time: 135ms, Memory: 10MB, Status: Runtime Error - Codetree

---
+++ 260303/아름다운 수열 2/beautiful-sequence-2.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int M = sc.nextInt();
+        int[] count = new int[101];
+        int[] count2 = new int[101];
+        int[] A = new int[N];
+        for (int i = 0; i < N; i++)
+            A[i] = sc.nextInt();
+        int[] B = new int[M];
+        for (int i = 0; i < M; i++) {
+            B[i] = sc.nextInt();
+            count[B[i]]++;
+            count2[A[i]]++;
+        }
+
+        int answer = 0;
+        boolean flag = false;
+        for (int i = 1; i < 101; i++) {
+            if (count[i] != count2[i]) {
+                flag = true;
+                break;
+            }
+        }
+        if (!flag) answer++;
+        
+        for (int i = M; i < N; i++) {
+            flag = false;
+
+            count2[A[i]]++;
+            count2[A[i-M]]--;
+
+            for (int j = 1; j < 101; j++) {
+                if (count[j] != count2[j]) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (!flag) answer++;
+        }
+        
+        System.out.println(answer);
+    }
+}
