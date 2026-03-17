852ea8c8c8f5f29c2f8b11b6ea22e3d5c53dfa4c
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2026-03-17 22:32:03 +0900

    Title: 흰검 칠하기, Time: 173ms, Memory: 14MB, Status: Wrong Answer - Codetree

---
+++ 260317/흰검 칠하기/painting-white-black.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        int[][] grid = new int[(N*100*2 + 1)][2];
+        int curr = N*100;
+        for (int i = 0; i < N; i++) {
+            int x = sc.nextInt();
+            char d = sc.next().charAt(0);
+
+            int dir = 0;
+            if (d == 'R') dir = 1;
+            else dir = -1;
+
+            for (int j = 0; j < x; j++) {
+                if (d == 'R') {
+                    grid[curr][0] += 1;
+                    grid[curr][1] = 1;
+                }
+                else {
+                    grid[curr][0] += 10;
+                    grid[curr][1] = 10;
+                }
+                curr += dir;
+
+            }
+
+            curr -= dir;
+        }
+        
+        int white = 0;
+        int black = 0;
+        int grey = 0;
+        for (int i = 0; i < N*100*2 + 1; i++) {
+            if (grid[i][0] >= 22) grey++;
+            else if (grid[i][0] > 0 && grid[i][1] == 1) black++;
+            else if (grid[i][0] > 0 && grid[i][1] == 10) white++;
+        }
+
+        System.out.println(white + " " + black + " " + grey);
+    }
+}
