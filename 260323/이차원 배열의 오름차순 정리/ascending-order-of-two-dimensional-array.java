6270e5297afa741a385b47f2bf16f0c88c56dfee
DonghanPark <69855374+DonghanPark@users.noreply.github.com>
2026-03-23 23:34:58 +0900

    Title: 이차원 배열의 오름차순 정리, Time: 126ms, Memory: 10MB, Status: Passed - Codetree

--- 260323/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
+++ 260323/이차원 배열의 오름차순 정리/ascending-order-of-two-dimensional-array.java
@@ -1,19 +1,32 @@
 import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int k = sc.nextInt();
-        
-        ArrayList<Integer> nums =  new ArrayList<>(); 
-        for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= n; j++) {
-                nums.add(i*j);
+
+        long left = 1;
+        long right = (long) n * n;
+        long answer = 0;
+
+        while (left <= right) {
+            long mid = (left + right) / 2;
+
+            long count = 0;
+
+            for (int i = 1; i <= n; i++) {
+                count += Math.min(n, mid / i);
+            }
+
+            if (count >= k) {
+                answer = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
 
-        Collections.sort(nums);
-
-        System.out.println(nums.get(k - 1));
+        System.out.println(answer);
     }
 }
